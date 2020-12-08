6193b4d5743fa0e7cc0baa44dddafcd6632c4440
ElisabetKra <74307256+ElisabetKra@users.noreply.github.com>
2020-12-08 09:34:42 +0100

    kalender

---
+++ script.js
@@ -0,0 +1,42 @@
+const date = new Date();
+let month= date.getMonth();
+
+const months = [
+	"Januari",
+	"Februari",
+	"Mars",
+	"April",
+	"Maj",
+	"Juni",
+	"Juli",
+	"Augusti",
+	"September",
+	"Oktober",
+	"November",
+	"December"
+];
+
+console.log(document.querySelector(".month").innerHTML);
+
+document.querySelector(".month").innerHTML
+= months[month];
+
+let days = "";
+
+for(let i = 1;i <= 31; i++){
+/* 
+	days +=´<div>${i}</div<´;
+	*/
+}
+
+function left(){
+	month=month-1;
+	document.querySelector(".month").innerHTML
+= months[month];
+}
+
+function right(){
+	month=month+1;
+	document.querySelector(".month").innerHTML
+= months[month];
+}
